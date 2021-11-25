c91f1376167454725129b6a53310b62a23b678aa
adarsh <adarshdhote8624@gmail.com>
2021-11-25 13:52:09 +0530

    add cors

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+const cors = require('cors');
+// Cors 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+}
 
+app.use(cors(corsOptions))
 app.use(express.static('public'));
 
 const connectDB = require('./config/db');
 connectDB();
 
-const cors = require('cors');
-// Cors 
-const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
-app.use(cors(corsOptions));
-
-// Templates 
 app.use(express.json());
 
 app.set('views', path.join(__dirname, '/views'));
@@ -28,6 +25,5 @@ app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
 
-app.listen(PORT, () => {
-    console.log('Listening to port $(PORT)');
-})
+
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));
