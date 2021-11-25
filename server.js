e874eeaacbf9872a568c97b44988da4b76c54cb8
adarsh <adarshdhote8624@gmail.com>
2021-11-25 13:12:01 +0530

    add cors

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.use(express.static('public'));
 const connectDB = require('./config/db');
 connectDB();
 
+const cors = require('cors');
+// Cors 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
+app.use(cors(corsOptions))
+
 // Templates 
 app.use(express.json());
 
