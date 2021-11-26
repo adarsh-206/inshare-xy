b95bd214b6dbbbff81cc053bb1ce4ba592ab4e28
adarsh8878 <66101827+adarsh8878@users.noreply.github.com>
2021-11-26 22:11:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
 
 const cors = require('cors');
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://adarsh8878.github.io', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
