f75af6cf3027e5a05202179e82f819671e80920b
adarsh8878 <66101827+adarsh8878@users.noreply.github.com>
2021-11-26 20:58:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 // Routes 
-app.use('/', require('./index.html'));
+app.use('/index', require('./index.html'));
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
