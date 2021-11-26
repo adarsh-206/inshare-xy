e3d33659d6b4c8e0f70fec9e7968425dc597c514
adarsh8878 <66101827+adarsh8878@users.noreply.github.com>
2021-11-26 18:16:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
-const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
@@ -10,10 +9,13 @@ app.use(express.json());
 const connectDB = require('./config/db');
 connectDB();
 
-
-app.use(cors({
-    origin: "'http://localhost:3000', 'http://localhost:5000', 'http://localhost:3001'"
-}));
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 
 // Templates
