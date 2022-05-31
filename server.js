37f62113e365d1b02986d803def97b36ecf38868
techie-adarsh <66101827+techie-adarsh@users.noreply.github.com>
2022-05-31 15:44:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB();
 const cors = require('cors');
 const corsOptions ={ 
     origin:'https://techie-adarsh.github.io',                           /* 'https://techie-adarsh.github.io' */
-    credentials:true,            //access-control-allow-credentials:true
+    credentials:false,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
